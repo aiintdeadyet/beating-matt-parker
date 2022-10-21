86342ab886a85729933f1eb075cf1547e81b4e35
aiin'tdeadyet <47235002+aiintdeadyet@users.noreply.github.com>
2022-10-21 12:08:59 -0600

    added timing and stripping away extra chars

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Trying make better code than Matt parker made in this video https://www.youtube.com/watch?v=c33AZBnRHks&t=74s
 
 ## Rules
-Must be coded in such a way that Matt Parker could have coded this on his own. 
-This means in python, without speshal librarrys, can only use basic featurs that can be learned after a few minuts of googling. 
+
+Must be coded in such a way that Matt Parker could have coded this on his own.
+This means in python, without speshal librarrys, can only use basic featurs that can be learned after a few minuts of googling. All words must be words that are acualy in the english language (We will be using a very liberal deffinition of words in the english language but if you want a more strict deffinition you can just swap the word list).
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 # trying to find 5 words each containing 5 letters with no letter repeated 
+import re, time
 
 
-def take5(in_file):
+def narrow_list(in_file):
     '''takes a word list file returns a set of only 5 letter words'''
     f = open(in_file, 'r') # opens file of all english words
     all_words = f.readlines() # reads in word list
-    word_set = set()
+    word_set = set() # makes empty set that will be returned
     for word in all_words:
-        strip_word = word.strip().lower() # gets rid of all extra char
+        strip_word = word.lower() # gets rid of all extra char
+        strip_word = re.sub('[\W_]+', '', strip_word)
         # adds word to set of words
-        if len(strip_word) == 5: 
-            word_set.add(strip_word)
+        if len(strip_word) != 5: 
+            continue
+        word_set.add(strip_word)
 
     return word_set
 
 
 def main():
     '''runs program'''
-    print(take5("wordlist_all.txt"))
+    start_time = time.time()
+
+    print(narrow_list("wordlist_all.txt"))
+
+    end_time = time.time()
+    print("this program took", end_time - start_time, "seconds to run")
 
 
 if __name__ == "__main__":
