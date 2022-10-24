9008e6a71ae9aef24f168a813fdee83a42d94897
aiin'tdeadyet <47235002+aiintdeadyet@users.noreply.github.com>
2022-10-23 20:21:21 -0600

    transfer to desktop

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# trying to find 5 words each containing 5 letters with no letter repeated 
+# trying to find 5 words each containing 5 letters with no letter repeated
 import re, time
 
 
 def narrow_list(in_file):
-    '''takes a word list file returns a set of only 5 letter words'''
-    f = open(in_file, 'r') # opens file of all english words
-    all_words = f.readlines() # reads in word list
-    word_set = set() # makes empty set that will be returned
+    """takes a word list file returns a set of only 5 letter words"""
+    f = open(in_file, "r")  # opens file of all english words
+    all_words = f.readlines()  # reads in word list
+    word_set = set()  # makes empty set that will be returned
     for word in all_words:
-        strip_word = word.lower() # gets rid of all extra char
-        strip_word = re.sub('[\W_]+', '', strip_word)
+        strip_word = word.lower()  # gets rid of all extra char
+        strip_word = re.sub("[\W_]+", "", strip_word)
         # adds word to set of words
-        if len(strip_word) != 5: 
+        if len(strip_word) != 5:
             continue
         word_set.add(strip_word)
 
@@ -19,10 +19,11 @@ def narrow_list(in_file):
 
 
 def main():
-    '''runs program'''
+    """runs program"""
     start_time = time.time()
 
-    print(narrow_list("wordlist_all.txt"))
+    narrowed = narrow_list("wordlist_all.txt")
+    print(len(narrowed))
 
     end_time = time.time()
     print("this program took", end_time - start_time, "seconds to run")
---
+++ parker_list.py
@@ -0,0 +1,10 @@
+# contains the parker_list class 
+
+
+class Parker_list:
+    '''Parker_list objects alow for more efficient use of data'''
+    def __init__(self, words=[], letters=[]):
+        '''Initializes Parker_list objects'''
+        self.Words = words
+        self.Letters = letters
+        self.Len = len(words) 
