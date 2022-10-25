fec634ef3dd2552c7c2ffb3be940601ff57d8e2e
aiin'tdeadyet <47235002+aiintdeadyet@users.noreply.github.com>
2022-10-25 14:34:05 -0600

    added check word function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # trying to find 5 words each containing 5 letters with no letter repeated
-import re, time
+import time
 
 
 def narrow_list(in_file):
@@ -35,19 +35,59 @@ def narrow_list(in_file):
         "z": set(),
     }
     for word in all_words:
-        strip_word = word.lower().strip()  # gets rid of all extra char
-        # strip_word = re.sub("[\W_]+", "", strip_word)
+        strip_word = word.lower().strip("\n")  # gets rid of all extra char
+
         # adds word to set of words
-        if len(strip_word) != 5:
+        if (len(strip_word) != 5) or not check_word(strip_word):
             continue
-        
         for char in strip_word:
-            try: # if char in word dict
-                word_dict[char].add(strip_word)
-            except: # else forget about it
-                break
+            # try:  # if char in word dict
+            word_dict[char].add(strip_word)
+            # except:  # else forget about it
+            #     break
 
     return word_dict
+
+
+def check_word(word):
+    """checks that the word only has letters that don't repeat"""
+    char_list = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
+    used = []  # used char
+    for char in word:
+        if (
+            char not in char_list or char in used
+        ):  # if char is a letter we haven't used yet
+            return False
+        used.append(char)
+
+    return True
 
 
 def main():
--- parker_list.py
+++ parker_list.py
@@ -1,17 +1,51 @@
-# contains the parker_list class 
+# contains the parker_list class
+
+
+from os import remove
+from turtle import update
 
 
 class Parker_list:
-    '''Parker_list objects alow for more efficient use of data'''
-    def __init__(self, words=[], letters=[]):
-        '''Initializes Parker_list objects'''
+    """Parker_list objects alow for more efficient use of data"""
+
+    def __init__(self, words=[]):
+        """Initializes Parker_list objects"""
         self.Words = words
-        self.Letters = letters
-        self.Len = len(words) 
+        self.Letters = self.used_letters()
+
+    def used_letters(self):
+        """returns a set of letters that have been used in the words"""
+        re_set = set()
+        for word in self.Words:
+            for char in word:
+                re_set.add(char)
+        return re_set
+
+    def add(self, word):
+        """Add word to the object"""
+        self.Words.append(word)
+        self.Letters = self.used_letters()
+
+    def remove(self, word):
+        """Removes a word from the object using either the index or the value of the removed word"""
+        if word == str:
+            self.Words.remove(word)
+            self.Letters = self.used_letters()
+
+    def __len__(self):
+        """Returns how manny words are in the object"""
+        return len(self.Words)
+
+    def __str__(self):
+        """Returns a string representation of the object"""
+        return f"Words {str(self.Words)}\nletters used {str(self.Letters)}"
 
 
 def main():
-    pass
+    park_list = Parker_list(["james", "david"])
+    park_list.add("critc")
+    park_list.remove("james")
+    print(park_list)
 
 
 if __name__ == "__main__":
